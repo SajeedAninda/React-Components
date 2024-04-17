ef54b77dc1eb832185e137fe612624c69871cb04
SajeedAninda <sajaninda@gmail.com>
2024-04-17 23:09:51 +0600

    Set tranition type

--- src/Components/Switch.jsx
+++ src/Components/Switch.jsx
@@ -5,8 +5,8 @@ const Switch = () => {
 
     return (
         <div className='bg-blue-300 h-screen flex justify-center items-center'>
-            <div onClick={() => { setSwitchOn(!switchOn) }} className={`bg-gray-600 h-32 w-[25%] rounded-full flex relative transition-all duration-300 ${switchOn? `bg-green-300` : ``}`}>
-                <span className={`h-32 w-32 transition-all duration-500 rounded-full bg-white shadow-lg shadow-slate-500 cursor-pointer 
+            <div onClick={() => { setSwitchOn(!switchOn) }} className={`bg-gray-600 h-32 w-[25%] rounded-full flex relative transition-all ease-in-out duration-300 ${switchOn? `bg-green-300` : ``}`}>
+                <span className={`h-32 w-32 transition-all ease-in-out duration-500 rounded-full bg-white shadow-lg shadow-slate-500 cursor-pointer 
                 ${switchOn? `bg-green-700 absolute right-0` : ``}
                 `} />
             </div>
