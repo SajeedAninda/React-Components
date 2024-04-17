9104811f5f0fd847bff2dc03b438ec76b8bce420
SajeedAninda <sajaninda@gmail.com>
2024-04-17 14:45:02 +0600

    Created a state and handler

--- src/Components/Dropdown.jsx
+++ src/Components/Dropdown.jsx
@@ -4,19 +4,21 @@ import avatar from "../assets/Image/avatar.jpg";
 const Dropdown = () => {
     let menus = ["Home", "Profile", "Settings", "Accessibility", "Shop", "Fashion"];
 
-    
+    let [open, setOpen] = useState(false)
 
     return (
         <div className='h-screen bg-gray-100 flex pt-14 flex-col items-center'>
-            <img className='w-[100px] rounded-full h-[100px] cursor-pointer hover:shadow-2xl' src={avatar} alt="" />
+            <img onClick={()=>{setOpen(!open)}} className='w-[100px] rounded-full h-[100px] cursor-pointer hover:shadow-2xl' src={avatar} alt="" />
 
-            <div>
-                <ul className='p-8 bg-white mt-4 rounded-xl shadow-xl'>
-                    {menus.map(menu => (
-                        <li key={menu} className='text-xl hover:bg-blue-300 py-2 px-4 rounded-xl cursor-pointer font-semibold text-center pt-2'>{menu}</li>
-                    ))}
-                </ul>
-            </div>
+            {
+                open && <div>
+                    <ul className='p-8 bg-white mt-2 rounded-xl shadow-xl'>
+                        {menus.map(menu => (
+                            <li key={menu} className='text-xl hover:bg-blue-300 py-2 px-4 rounded-xl cursor-pointer font-semibold text-center pt-2'>{menu}</li>
+                        ))}
+                    </ul>
+                </div>
+            }
         </div>
     );
 };
