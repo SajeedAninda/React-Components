40c61c5a4ad041ff67a996d2d7b2c3ffbf6df9cf
SajeedAninda <sajaninda@gmail.com>
2024-04-17 22:57:49 +0600

    Set a bg color for the div

--- src/Components/Switch.jsx
+++ src/Components/Switch.jsx
@@ -5,7 +5,7 @@ const Switch = () => {
 
     return (
         <div className='bg-blue-300 h-screen flex justify-center items-center'>
-            <div onClick={() => { setSwitchOn(!switchOn) }} className='bg-gray-600 h-32 w-[25%] rounded-full flex relative'>
+            <div onClick={() => { setSwitchOn(!switchOn) }} className={`bg-gray-600 h-32 w-[25%] rounded-full flex relative ${switchOn? `bg-green-300` : ``}`}>
                 <span className={`h-32 w-32 rounded-full bg-white shadow-lg shadow-slate-500 cursor-pointer 
                 ${switchOn? `bg-green-700 absolute right-0` : ``}
                 `} />
