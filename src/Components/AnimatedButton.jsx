972436f3049f075008747182658dee6211e547c8
SajeedAninda <sajaninda@gmail.com>
2024-04-18 17:56:43 +0600

    Designed the button

--- src/Components/AnimatedButton.jsx
+++ src/Components/AnimatedButton.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaGithub } from "react-icons/fa";
+
 
 const AnimatedButton = () => {
+    let [hovered, setHovered] = useState(false);
+
     return (
-        <div className='h-screen bg-red-200'>
-            
+        <div className='h-screen bg-red-200 flex justify-center items-center'>
+            <button className='p-4 rounded-xl text-white flex items-center bg-gray-700 gap-2'>
+                <FaGithub className='text-4xl' />
+                <div className='overflow-x-hidden transition-all duration-300 ease-out'>
+                    <span className='text-2xl font-bold'>Github</span>
+                </div>
+            </button>
         </div>
     );
 };
