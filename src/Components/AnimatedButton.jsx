3e4049dd92bb81eb5736c9da0ffb4f231a655007
SajeedAninda <sajaninda@gmail.com>
2024-04-18 18:00:50 +0600

    Completed the transition hover of button

--- src/Components/AnimatedButton.jsx
+++ src/Components/AnimatedButton.jsx
@@ -1,19 +1,23 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { FaGithub } from "react-icons/fa";
 
 
 const AnimatedButton = () => {
     let [hovered, setHovered] = useState(false);
+    let buttonRef = useRef(null);
 
     return (
         <div className='h-screen bg-red-200 flex justify-center items-center'>
             <button
                 onMouseEnter={() => setHovered(true)}
                 onMouseLeave={() => setHovered(false)}
-                className='p-4 rounded-xl text-white flex items-center bg-gray-700 gap-2'>
+                className='p-4 rounded-xl text-white flex items-center bg-gray-700'>
                 <FaGithub className='text-4xl' />
-                <div className='overflow-x-hidden transition-all duration-300 ease-out'>
-                    <span className='text-2xl font-bold'>Github</span>
+                <div
+                    style={{ width: hovered ? buttonRef.current?.offsetWidth || 0 : 0 }}
+                    className="overflow-x-hidden transition-all duration-300 ease-out"
+                >
+                    <span ref={buttonRef} className='text-2xl font-bold px-2'>Github</span>
                 </div>
             </button>
         </div>
