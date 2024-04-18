339dfdc089055fecff56516aa9732e3f11363f46
SajeedAninda <sajaninda@gmail.com>
2024-04-18 17:57:55 +0600

    Created a state

--- src/Components/AnimatedButton.jsx
+++ src/Components/AnimatedButton.jsx
@@ -7,7 +7,10 @@ const AnimatedButton = () => {
 
     return (
         <div className='h-screen bg-red-200 flex justify-center items-center'>
-            <button className='p-4 rounded-xl text-white flex items-center bg-gray-700 gap-2'>
+            <button
+                onMouseEnter={() => setHovered(true)}
+                onMouseLeave={() => setHovered(false)}
+                className='p-4 rounded-xl text-white flex items-center bg-gray-700 gap-2'>
                 <FaGithub className='text-4xl' />
                 <div className='overflow-x-hidden transition-all duration-300 ease-out'>
                     <span className='text-2xl font-bold'>Github</span>
